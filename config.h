dc3450a5548d91a32be60942c2ed5abe5365c9c9
Ismael J. Tisminetzky <me@ismaeltisminetzky.xyz>
2026-07-06 14:05:39 -0300

    knsip

--- config.h
+++ config.h
@@ -5,13 +5,15 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14" };
+/* static const char *fonts[]          = { "monospace:size=14" }; */
+static const char *fonts[]          = { "-*-noto mono-*-*-*-*-*-*-*-*-*-*-*-*:size=14" };
 static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#ffb86c";
 static const char col_gray3[]       = "#ff79c6";
 static const char col_gray4[]       = "#ffb86c";
 static const char col_cyan[]        = "#282a36";
+static const unsigned int refreshrate = 60; // Set this to your monitor's Hz (e.g., 60, 144, 240)
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -70,6 +72,7 @@ static Key keys[] = {
 	{ 0,                            XK_Print,  spawn,		       SHCMD("maim -s /tank_data_1/screenshots/pic-full-$(date '+%y%m%d-%H%M-%S').png") },
 	{ MODKEY|ShiftMask,			        XK_o,		   spawn,		       SHCMD("firefox") },
 	{ MODKEY|ShiftMask,			        XK_r,		   spawn,		       SHCMD("chrome") },
+  { MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("ksnip -r") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
--- config.mk
+++ config.mk
@@ -4,11 +4,11 @@ VERSION = 6.8
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = /usr
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/local/include
-X11LIB = /usr/local/lib
+X11INC = /usr/include
+X11LIB = /usr/lib
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
@@ -16,7 +16,7 @@ XINERAMAFLAGS = -DXINERAMA
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
-FREETYPEINC = /usr/local/include/freetype2
+FREETYPEINC = /usr/include/freetype2
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
 #MANPREFIX = ${PREFIX}/man
